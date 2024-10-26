f9661b77a3ba9feb8bb9df4be435c9e01f330cff
Mateusz Baginski <cziken58@gmail.com>
2024-10-26 18:19:22 +0200

    feat(frontend): add dockerfile

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,6 +1,11 @@
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  output: 'hybrid',
   integrations: [tailwind()],
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
