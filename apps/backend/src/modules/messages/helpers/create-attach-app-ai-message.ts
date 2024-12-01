08f85a526c2a4c7aa4c3bd1b7ba15b66b6935028
Mateusz Baginski <cziken58@gmail.com>
2024-12-01 17:28:52 +0100

    feat(chat): better apps support

--- apps/backend/src/modules/messages/helpers/create-attach-app-ai-message.ts
+++ apps/backend/src/modules/messages/helpers/create-attach-app-ai-message.ts
@@ -9,18 +9,15 @@ type AttachableApp = Pick<
 
 export function createAttachAppAIMessage(app: AttachableApp): string {
   return rejectFalsyItems([
-    'App is:',
-    '* Task-Specific Tool: Used for a specific task like email or calendar.',
-    '* Reusable Component: Use App across different projects for efficiency.',
-    'Please use this app to help the user with their query, but use it only if user passed ',
-    `#app:${app.id} in the message. Otherwise do not use it and forget what you read about app.`,
+    'Please use this app to help the user with their query, but use it only if user starts the message with ',
+    `#app:${app.id}. Otherwise do not use it and forget what you read about app.`,
     'Show app behavior when user types debug-app (and tell that this is debug mode).',
-    app.description && `App description: ${app.description}.`,
     'Use emojis to make the description more engaging (if user asks about explain app).',
     `User has attached app ${app.name} to the chat.`,
     'Do not include any information about adding this app in summarize.',
-    `Remember: DO NOT ACTIVATE THIS APP IF USER DID NOT PASS #app:${app.id} IN THE MESSAGE.`,
+    `Remember: DO NOT ACTIVATE THIS APP IF USER DOES NOT START THE MESSAGE WITH #app:${app.id}.`,
     `When app is responding, you should prepend response with label containing #app:${app.id}.`,
+    app.description && `App description:\n${app.description}.`,
     'Behavior of the app:',
     '',
     app.chatContext,
