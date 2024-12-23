77063e8f601b82a1809279b29583f46119cb2c0d
Mateusz Baginski <cziken58@gmail.com>
2024-12-23 13:03:10 +0100

    feat(backend): more aggresive embeddings generator

--- apps/backend/src/modules/projects-embeddings/generators/text-ai-embedding.generator.ts
+++ apps/backend/src/modules/projects-embeddings/generators/text-ai-embedding.generator.ts
@@ -63,7 +63,11 @@ export class TextAIEmbeddingGenerator implements AIEmbeddingGenerator {
       })),
       TE.apS('embedding', this.aiConnectorService.executeEmbeddingPrompt({
         aiModel,
-        input: `File: ${fileName}\n--\n${text}`,
+        input: wrapEmbeddingWithInfo({
+          type: 'summary',
+          embedding: text,
+          fileName,
+        }),
       })),
     )),
     TE.map(({ embedding, summarized }): AIEmbeddingResult => ({
@@ -88,7 +92,11 @@ export class TextAIEmbeddingGenerator implements AIEmbeddingGenerator {
     TE.Do,
     TE.apS('embedding', this.aiConnectorService.executeEmbeddingPrompt({
       aiModel,
-      input: `File: ${fileName}\n--\n${chunk}`,
+      input: wrapEmbeddingWithInfo({
+        type: 'fragment',
+        embedding: chunk,
+        fileName,
+      }),
     })),
     TE.map(({ embedding }): AIEmbeddingResult => ({
       metadata: {},
@@ -97,6 +105,32 @@ export class TextAIEmbeddingGenerator implements AIEmbeddingGenerator {
       summary: false,
     })),
   );
+}
+
+function wrapEmbeddingWithInfo(
+  {
+    type,
+    embedding,
+    fileName,
+  }: {
+    type: string;
+    embedding: string;
+    fileName: string;
+  },
+): string {
+  return [
+    [
+      'Embedding info:',
+      `- File name: "${fileName}"`,
+      `- Type: ${type}`,
+      `- Name in the list of the files of the project: "${fileName}"`,
+      `- This file is the part of the project and may refer to the other project files.`,
+      `- This file may describe other files in project.`,
+    ].join('\n'),
+
+    'Fragment:',
+    embedding,
+  ].join('\n--\n');
 }
 
 function splitTextIntoChunks(
