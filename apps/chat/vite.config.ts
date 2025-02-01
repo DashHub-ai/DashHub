0e23a66679bfc3a2996d3faf51382f129136ecbe
Mateusz Baginski <cziken58@gmail.com>
2025-02-01 13:14:40 +0100

    fix(chat): change base url for assets

--- apps/chat/vite.config.ts
+++ apps/chat/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/app/' : '/',
+  base: '/',
   server: {
     host: true,
     port: 5173,
