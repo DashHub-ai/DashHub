7f0ccbf80124f1b9641d464bb9058fe3fdb9d272
Mateusz Baginski <cziken58@gmail.com>
2025-02-09 14:46:35 +0100

    feat(chat): display avatar in navigation header

--- apps/chat/src/layouts/navigation/logged-in/logged-in-button.tsx
+++ apps/chat/src/layouts/navigation/logged-in/logged-in-button.tsx
@@ -1,14 +1,32 @@
-import { UserIcon } from 'lucide-react';
+import { Loader2, UserIcon } from 'lucide-react';
+
+import { useSdkSubscribeMeOrThrow } from '@llm/sdk';
 
 export function LoggedInButton() {
+  const data = useSdkSubscribeMeOrThrow();
+
   return (
     <button
       type="button"
-      className="inline-flex relative justify-center items-center hover:bg-accent disabled:opacity-50 px-4 py-2 rounded-full focus-visible:ring-1 focus-visible:ring-ring w-8 h-8 font-medium text-sm hover:text-accent-foreground whitespace-nowrap transition-colors focus-visible:outline-none disabled:pointer-events-none"
+      className="inline-flex relative justify-center items-center hover:bg-accent disabled:opacity-50 px-4 py-2 rounded-full focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring w-8 h-8 font-medium text-sm whitespace-nowrap transition-colors hover:text-accent-foreground disabled:pointer-events-none"
       aria-haspopup="true"
     >
       <span className="relative flex justify-center items-center rounded-full w-10 h-10 overflow-hidden shrink-0 uk-background-secondary">
-        <UserIcon size={16} />
+        {data.loading
+          ? (
+              <Loader2 className="w-4 h-4 animate-spin" />
+            )
+          : data.me.avatar
+            ? (
+                <img
+                  src={data.me.avatar.publicUrl}
+                  alt="Profile"
+                  className="border border-border w-full h-full object-contain"
+                />
+              )
+            : (
+                <UserIcon size={16} />
+              )}
       </span>
     </button>
   );
