5c37ef808ca361dbdc75ea4d95484f53c0728082
Mateusz Baginski <cziken58@gmail.com>
2025-02-22 16:51:44 +0100

    feat(chat): improve styling

--- apps/chat/src/modules/pinned-messages/grid/pinned-messages-timeline.tsx
+++ apps/chat/src/modules/pinned-messages/grid/pinned-messages-timeline.tsx
@@ -15,35 +15,30 @@ export function PinnedMessagesTimeline({ items }: TimelineProps) {
     <div className="relative">
       <div className="top-0 bottom-0 left-1/2 absolute bg-gray-200 w-0.5 -translate-x-1/2" />
 
-      <div className="relative space-y-12">
+      <div className="relative">
         {items.map((item, index) => {
           const isEven = index % 2 === 0;
 
           return (
-            <div key={item.id} className="relative">
-              <div className="top-7 left-1/2 absolute bg-white border-2 border-gray-300 rounded-full w-3 h-3 -translate-x-1/2" />
+            <div key={item.id} className="relative" style={{ marginTop: index === 0 ? '2rem' : '-4rem' }}>
+              <div className="top-[29px] left-1/2 z-10 absolute bg-white border-2 border-gray-300 rounded-full w-3 h-3 -translate-x-1/2" />
 
-              <div className={clsx('flex items-start', isEven ? 'justify-end pr-[52%]' : 'justify-start pl-[52%]')}>
+              <div className="top-[29px] left-1/2 absolute -translate-x-1/2 -translate-y-12">
+                <span className="bg-gray-50 px-3 py-1 rounded-full text-gray-600 text-xs">
+                  {formatDate(item.createdAt)}
+                </span>
+              </div>
+
+              <div className={clsx('flex items-start', isEven ? 'justify-end pr-[56%]' : 'justify-start pl-[56%]')}>
                 <div
-                  className="top-1/2 left-1/2 absolute bg-gray-200 w-4 h-0.5"
+                  className="top-[34px] left-1/2 absolute bg-gray-200 w-4 h-0.5"
                   style={{
                     transform: `translateX(${isEven ? '-100%' : '0'})`,
                   }}
                 />
 
                 <div className="w-full max-w-xl">
-                  <div className="clear-both">
-                    <PinnedMessageCard pinnedMessage={item} />
-                  </div>
-
-                  <div
-                    className={clsx(
-                      'inline-block bg-gray-50 px-3 pt-4 rounded-full text-gray-600 text-sm',
-                      isEven ? 'float-right' : 'float-left',
-                    )}
-                  >
-                    {formatDate(item.createdAt)}
-                  </div>
+                  <PinnedMessageCard pinnedMessage={item} />
                 </div>
               </div>
             </div>
