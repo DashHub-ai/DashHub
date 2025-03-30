83736b4f66979b682cf2b2da7defb74d9c03c867
Mateusz Baginski <cziken58@gmail.com>
2025-03-30 17:35:40 +0200

    feat(chat): better overflow of chats grid

--- apps/chat/src/routes/home/home.route.tsx
+++ apps/chat/src/routes/home/home.route.tsx
@@ -8,18 +8,23 @@ export function HomeRoute() {
   const t = useI18n().pack.routes.home;
 
   return (
-    <PageWithSidebarLayout contentClassName="flex align-stretch">
+    <PageWithSidebarLayout
+      contentClassName="flex align-stretch"
+      withFooter={false}
+    >
       <RouteMetaTags meta={t.meta} />
 
-      <div className="grid grid-rows-[1fr_auto] mx-auto w-full max-w-5xl">
-        <div className="flex flex-col justify-center">
-          <PromotedAppsContainer
-            title={t.exploreApps}
-            className="mb-16 w-full min-h-[490px]"
-          />
+      <div className="gap-8 grid grid-rows-[1fr_auto] mx-auto w-full max-w-5xl">
+        <div className="relative">
+          <div className="top-0 right-0 left-0 absolute flex flex-col justify-start [@media(min-height:825px)]:justify-center h-full overflow-y-auto">
+            <PromotedAppsContainer
+              title={t.exploreApps}
+              className="mb-16 w-full"
+            />
+          </div>
         </div>
 
-        <StartChatForm className="mt-auto" />
+        <StartChatForm className="mt-auto pb-4" />
       </div>
     </PageWithSidebarLayout>
   );
