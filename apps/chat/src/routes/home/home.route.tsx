a7d039c951cfe0b3dc2b853df498f95712dad63b
Mateusz Baginski <cziken58@gmail.com>
2025-03-30 18:06:44 +0200

    feat(chat): improve breakpoints

--- apps/chat/src/routes/home/home.route.tsx
+++ apps/chat/src/routes/home/home.route.tsx
@@ -16,7 +16,7 @@ export function HomeRoute() {
 
       <div className="gap-5 grid grid-rows-[1fr_auto] mx-auto w-full max-w-5xl">
         <div className="relative">
-          <div className="top-0 right-0 left-0 absolute flex flex-col justify-start [@media(min-height:825px)]:justify-center h-full overflow-y-auto">
+          <div className="top-0 right-0 left-0 absolute flex flex-col justify-start [@media(min-height:870px)]:justify-center h-full overflow-y-auto">
             <PromotedAppsContainer
               title={t.exploreApps}
               className="mb-16 w-full"
