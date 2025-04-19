38f314a13a626773ce1fdd02e0f41c325e0b7be8
Mateusz Baginski <cziken58@gmail.com>
2025-04-19 15:43:43 +0200

    feat(chat): improve promotion bar layout

--- apps/chat/src/routes/home/home.route.tsx
+++ apps/chat/src/routes/home/home.route.tsx
@@ -19,7 +19,7 @@ export function HomeRoute() {
     >
       <RouteMetaTags meta={t.meta} />
 
-      <div className="z-10 relative gap-5 grid grid-rows-[1fr_auto] mx-auto w-full max-w-5xl">
+      <div className="z-10 relative gap-5 grid grid-rows-[1fr_auto] mx-auto w-full max-w-6xl">
         <div className="relative">
           <div className="top-0 right-0 left-0 absolute flex flex-col justify-start [@media(min-height:980px)]:justify-center h-full overflow-y-auto">
             <div className="mb-8 [@media(max-height:980px)]:pt-5">
