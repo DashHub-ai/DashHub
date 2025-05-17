5db3cad50bac2b3bb102af9de28d20ea6b0ee631
Mateusz Baginski <cziken58@gmail.com>
2025-05-17 16:26:17 +0200

    feat(sdk): add fallback for UUID

--- packages/sdk/CHANGELOG.md
+++ packages/sdk/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @dashhub/sdk
 
+## 1.0.2
+
+### Patch Changes
+
+- Add fallback for UUID generator
+
 ## 1.0.1
 
 ### Patch Changes
--- packages/sdk/src/shared/dto/sdk-table-row-uuid.dto.ts
+++ packages/sdk/src/shared/dto/sdk-table-row-uuid.dto.ts
@@ -1,5 +1,12 @@
+import { v4 } from 'uuid';
 import { z } from 'zod';
 
-export const SdkTableRowUuidV = z.coerce.string();
+export const SdkTableRowUuidV = z.coerce.string().transform(
+  // Fix quirk, coercing `undefined` string results in `undefined` value.
+  // It's better to generate a new UUID in this case.
+  value => value === 'undefined'
+    ? v4()
+    : value,
+);
 
 export type SdkTableRowUuidT = z.infer<typeof SdkTableRowUuidV>;
